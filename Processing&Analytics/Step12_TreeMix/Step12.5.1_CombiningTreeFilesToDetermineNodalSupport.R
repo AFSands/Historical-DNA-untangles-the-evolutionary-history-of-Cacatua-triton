001156805f5fa274de3ed897e5fa4b9e62100b04
AFSands <francis.sands@yahoo.com>
2026-02-03 16:42:46 +0800

    Create Step12.5.1_CombiningTreeFilesToDetermineNodalSupport.R
    This R script uses the outputs of Step12.2 to merge individual tree files into a single trees file (i.e. merging bootstrap replicates), which can be used to determine support in Step12.5.2.

---
+++ Processing&Analytics/Step12_TreeMix/Step12.5.1_CombiningTreeFilesToDetermineNodalSupport.R
@@ -0,0 +1,38 @@
+#################################################
+# NOTES: This script was run in RStudio. See the associated study for information on the R version and further details. This script uses the outputs of Step12.2 to merge individual tree files into a single trees file (i.e. merging bootstrap replicates), which can be used to determine support in Step12.5.2.
+#################################################
+
+
+# -------------------------------
+# Set directory
+# -------------------------------
+dir_path <- "C:/Users/Franc/Documents/Hong Kong University Work/Projects/Cockatoos/Paper2/Results/TreeMix/Revised/Regions/CMrooted/Results"
+
+# -------------------------------
+# List all *.3.treeout.gz files
+# -------------------------------
+files <- list.files(
+  path = dir_path,
+  pattern = "\\.3\\.treeout\\.gz$",
+  full.names = TRUE
+)
+
+# -------------------------------
+# Read first line from each gz file
+# -------------------------------
+get_first_line <- function(file) {
+  con <- gzfile(file, open = "rt")
+  line <- readLines(con, n = 1)
+  close(con)
+  line
+}
+
+trees <- vapply(files, get_first_line, character(1))
+
+# -------------------------------
+# Write trees only (one per line)
+# -------------------------------
+writeLines(
+  text = trees,
+  con = file.path(dir_path, "TreeMix_first_line_trees.txt")
+)
